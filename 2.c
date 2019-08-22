239d2b5f9cd8398b039e8e9820b0b9f12109b0c9
Kautuk Raj <spamkraj@gmail.com>
2019-08-22 22:09:10 +0530

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include <conio.h>
+int main()
+{
+    char s[50];
+    int l,k,c=0,f=0;
+    int i=0;int res=1;int j=0;
+    char arr[10][10];
+    scanf("%s", s);
+    int len = strlen(s);
+    scanf("%d", &l);
+
+    while (i<=(len-l))
+    {
+        k=i;
+        for (j=1; j<=l; j++)
+        {
+            arr[i][j-1] = s[k];
+            k++;
+        }
+        i++;
+    }
+
+
+    for (i=0; i<len-1; i++)
+    {
+        for (j=2; j<10; j++)
+        {
+            arr[i][j]='\0';
+        }
+    }
+
+    int unique = 1;
+    for(int i = 0; i < l-1 ; i++)
+    {
+        if(strcmp (arr[i],arr[i+1]) == 0)
+            continue;
+        else
+            unique++;
+    }
+    printf("The number of unique elements are %d ",unique);
+
+    return 0;
+}
