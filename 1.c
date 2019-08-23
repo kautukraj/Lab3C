db0d389ba266bb2f3d49ac5da49a56de24a7b8bb
Kautuk Raj <spamkraj@gmail.com>
2019-08-23 08:30:45 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main()
+{
+
+
+    int r1,r2,x1,x2,y1,y2,d;
+    scanf ("%d %d %d %d %d %d", &x1, &y1, &x2, &y2, &r1, &r2);
+    d = sqrt ((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
+    if (r1<0 || r2<0)
+        printf("Invalid input\n");
+    else
+    {
+
+
+        if (d<= (r1+r2))
+            printf("YES\n");
+        else
+            printf("NO\n");
+    }
+
+    return 0;
+
+}
