d3eee0a55f67d9e658194c59f938e93039738321
Kautuk Raj <spamkraj@gmail.com>
2019-08-23 13:56:13 +0530

    Add files via upload

--- 1.c
+++ 1.c
@@ -5,7 +5,7 @@ int main()
 {
 
 
-    int r1,r2,x1,x2,y1,y2,d;
+    int r1,r2,x1,x2,y1,y2; float d=0;
     scanf ("%d %d %d %d %d %d", &x1, &y1, &x2, &y2, &r1, &r2);
     d = sqrt ((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
     if (r1<0 || r2<0)
