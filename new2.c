3c0ebc9894bb4b9754c761f0f8789e552dfb1c41
Kautuk Raj <spamkraj@gmail.com>
2019-08-23 14:29:35 +0530

    Add files via upload

--- new2.c
+++ new2.c
@@ -1,28 +1,21 @@
 // Count number of distinct substrings when length of substring is given
 #include <stdio.h>
-
 #include <string.h>
 
 int main()
 {
     char s[100], a[100][100];
     int i, j, l, n, k;
-    scanf("%s %i", s, &l);
+    scanf("%s %d", s, &l);
     n = strlen(s);
     int zero = 0;
     int one = 1;
     if (l > n)
-    {
-        printf("%d\n", zero);
-        return 0;
-    }
+    printf("%d\n", zero);
+
     else
     {
 
-        if (n == 1)
-            printf("%d\n", one);
-        else
-        {
             for (i = 0; i < n - 1; i++)
             {
                 k = 0;
@@ -32,6 +25,13 @@ int main()
                     k++;
                 }
             }
+
+            
+            for (i=0;i<10;i++)
+            printf("%s\n",a[i]);
+
+
+
             k = 0;
             for (i = 0; i < n - 1; i++)
             {
@@ -41,8 +41,9 @@ int main()
                         k++;
                 }
             }
-            printf("%i\n", n - l - (k / 2));
-        }
+            printf("%d\n", n - l - (k / 2));
+        
     }
+
     return 0;
 }
